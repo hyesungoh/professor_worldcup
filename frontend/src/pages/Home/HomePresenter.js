c80437e81d19b6dba4385b4e41f9fa210adbf914
hyesungoh <haesungoh414@gmail.com>
2021-03-22 00:55:15 +0900

    apply absoulte path, start using material-ui

--- frontend/src/pages/Home/HomePresenter.js
+++ frontend/src/pages/Home/HomePresenter.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Button from "@material-ui/core/Button";
+
+import "pages/Home/home.css";
+
 function HomePresenter() {
-    return <div>im home</div>;
+    return (
+        <main>
+            <Button variant="contained" color="primary">
+                HELLO WORLD
+            </Button>
+        </main>
+    );
 }
 
 export default HomePresenter;
