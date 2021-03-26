886a6b0cba1e3a66377c435f65684ef10aba6007
hyesungoh <haesungoh414@gmail.com>
2021-03-26 14:17:52 +0900

    add react-router-dom's link

--- frontend/src/pages/Home/HomePresenter.js
+++ frontend/src/pages/Home/HomePresenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 
 import "pages/Home/home.css";
@@ -10,6 +11,9 @@ function HomePresenter() {
             <Button variant="contained" color="primary">
                 HELLO WORLD
             </Button>
+
+            <Link to="/select">toSELECT</Link>
+            <Link to="/result">toRESULT</Link>
         </main>
     );
 }
