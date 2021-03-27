b0934004deccd0917cdb87a04f64a75422b79f95
hyesungoh <haesungoh414@gmail.com>
2021-03-27 23:14:24 +0900

    install node-sass

--- frontend/src/pages/Home/HomePresenter.js
+++ frontend/src/pages/Home/HomePresenter.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { MenuItem, Select } from "@material-ui/core";
 
 function HomePresenter() {
-    
     const majors = [
         "IT융합자율학부",
         "인문융합자율학부",
@@ -13,20 +12,28 @@ function HomePresenter() {
         "미디어컨텐츠융합자율학부",
     ];
 
+    const [myMajor, setMyMajor] = useState(majors[0]);
+
+    const onClick = (e) => {
+        setMyMajor(e.target.dataset.value);
+    };
+
     return (
         <main className="home">
-            <Select value={majors[0]}>
-                {majors.map((major) => (
-                    <MenuItem value={major}>{major}</MenuItem>
+            <Select value={myMajor}>
+                {majors.map((major, index) => (
+                    <MenuItem key={index} onClick={onClick} value={major}>
+                        {major}
+                    </MenuItem>
                 ))}
             </Select>
 
-            <Button variant="contained" color="primary">
+            {/* <Button variant="contained" color="primary">
                 HELLO WORLD
             </Button>
 
             <Link to="/select">toSELECT</Link>
-            <Link to="/result">toRESULT</Link>
+            <Link to="/result">toRESULT</Link> */}
         </main>
     );
 }
