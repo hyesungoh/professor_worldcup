356742504140bb9c3f91b8c77bc7ac7a9c9cd7e0
hyesungoh <haesungoh414@gmail.com>
2021-03-28 00:05:01 +0900

    add skhu background image

--- frontend/src/pages/Home/HomePresenter.js
+++ frontend/src/pages/Home/HomePresenter.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
 
+import background from "static/images/skhu_background.jpeg";
+
 function HomePresenter() {
     const majors = [
         "IT융합자율학부",
@@ -19,29 +21,36 @@ function HomePresenter() {
     };
 
     return (
-        <main className="home">
-            <FormControl>
-                <InputLabel id="major-label">전공을 선택해주세요</InputLabel>
-                <Select
-                    labelid="major-label"
-                    className="home__select"
-                    onChange={onChange}
-                    value={myMajor}
-                >
-                    <MenuItem value="">고민 좀 할게요</MenuItem>
-                    {majors.map((major, index) => (
-                        <MenuItem key={index} value={major}>
-                            {major}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
+        <main>
+            <div className="left">
+                <img className="background" src={background} alt="background" />
+            </div>
+            <div className="right home">
+                <FormControl>
+                    <InputLabel id="major-label">
+                        전공을 선택해주세요
+                    </InputLabel>
+                    <Select
+                        labelid="major-label"
+                        className="home__select"
+                        onChange={onChange}
+                        value={myMajor}
+                    >
+                        <MenuItem value="">고민 좀 할게요</MenuItem>
+                        {majors.map((major, index) => (
+                            <MenuItem key={index} value={major}>
+                                {major}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
 
-            <Link to="/select" className="home__btn">
-                <Button variant="contained" color="primary">
-                    HELLO WORLD
-                </Button>
-            </Link>
+                <Link to="/select" className="home__btn">
+                    <Button variant="contained" color="primary">
+                        우리과 최고의 교수님은?
+                    </Button>
+                </Link>
+            </div>
         </main>
     );
 }
